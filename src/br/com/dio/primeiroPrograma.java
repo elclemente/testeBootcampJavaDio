92c38e5021c9ac1a0dd0c8a0e50b90c20532adea
Ewerton Clemente <ewerton.clemente@gmail.com>
2023-02-21 21:52:40 -0300

    first commit

---
+++ src/br/com/dio/primeiroPrograma.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+import br.com.dio.model.gato;
+
+public class primeiroPrograma {
+    public static void main(String[] args) {
+        gato gato = new gato();
+
+        System.out.println(gato);
+
+
+        /*System.out.println("Hello World!!!");*/
+    }
+}
